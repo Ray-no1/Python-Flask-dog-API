36017e704856ca2e0de5e43d2645fce382e97620
Ray-no1 <145054201+Ray-no1@users.noreply.github.com>
2025-09-02 21:06:00 +0300

    Update dog_api.py

--- dog_api.py
+++ dog_api.py
@@ -5,32 +5,6 @@ import os
 
 load_dotenv()
 
-
-# def get_dog_info():
-#     url = f"https://api.thedogapi.com/v1/images/search"
-#     headers = {
-#         "x-api-key": os.getenv("API_KEY")
-#     }
-#     params = {
-#         "mime_types": "jpg,png",
-#         "has_breeds": "true",
-#         "limit": 1
-#     }
-# response = requests.get(url, headers=headers, params=params).json()
-# print(response)
-# breed = f"{response[0]['breeds'][0]['name']}"
-# weight = f"{response[0]['breeds'][0]['weight']['metric']} kilograms"
-# height = f"{response[0]['breeds'][0]['height']['metric']} centimeters"
-# bred_for = f"{response[0]['breeds'][0]['bred_for']}"
-# lifespan = f"{response[0]['breeds'][0]['life_span']}"
-# temperament = f"{response[0]['breeds'][0]['temperament']}"
-# img_url = f"{response[0]['url']}"
-# img_width = f"{response[0]['width']}"
-# img_height = f"{response[0]['height']}"
-
-# return breed, weight, height, bred_for, lifespan, temperament, img_url
-
-
 def get_dog_info():
     url = f"https://api.thedogapi.com/v1/images/search"
     headers = {
@@ -58,8 +32,6 @@ def get_dog_info():
                 "url": dog['url']
             })
     return dogs
-
-    # dog_1 = dogs_info[0]
 
 
 if __name__ == "__main__":
